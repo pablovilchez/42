43059e5481a3d7915d54f069a413bb7e53331a34
pablovilchez <pablovilchez.r@gmail.com>
2023-09-21 12:10:36 +0200

    ok

--- 01-Cursus/05-FdF/src/free_matrix.c
+++ 01-Cursus/05-FdF/src/free_matrix.c
@@ -1,24 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.c                                              :+:      :+:    :+:   */
+/*   free_matrix.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/19 17:42:23 by pvilchez          #+#    #+#             */
+/*   Created: 2023/09/21 12:01:58 by pvilchez          #+#    #+#             */
 /*   Updated: 2023/09/21 12:10:36 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(int argc, char *argv[])
-{
-	t_vertex	**matrix;
-	int			size[2];
-
-	matrix = get_matrix(argc, argv, size);
-	start_matrix(matrix, size);
-	print_matrix(matrix, size);
-	return (0);
-}
